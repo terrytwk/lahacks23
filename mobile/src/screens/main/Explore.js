537905bf371da339401b00634031adb49276481e
kenny wan <kennethzwan@gmail.com>
2023-04-23 00:55:13 -0700

    imports

--- mobile/src/screens/main/Explore.js
+++ mobile/src/screens/main/Explore.js
@@ -1,4 +1,25 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  ImageBackground,
+  Linking,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import colors from "../../theme/colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.textPrimary,
+  },
+});
 
 const Explore = () => {
   return (
@@ -9,3 +30,6 @@ const Explore = () => {
 };
 
 export default Explore;
+
+
+//use ai to recognize itineraries
