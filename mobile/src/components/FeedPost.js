468be9e2e5415371c93991795771e1cc59762ae9
Terry Kim <terrykim950@gmail.com>
2023-04-23 04:31:58 -0700

    finished feed page

--- mobile/src/components/FeedPost.js
+++ mobile/src/components/FeedPost.js
@@ -1,25 +1,124 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
 
 import PostInteraction from "./PostInteraction";
+import ItineraryCard from "./ItineraryCard";
 
-const FeedPost = ({ navigation }) => {
+const FeedPost = ({ navigation, post }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}></View>
-      <Text style={styles.title}>FeedPost</Text>
-      <Text style={styles.location}>location</Text>
-      <Text style={styles.location}>date</Text>
-      <PostInteraction navigation={navigation} icon_name="hop" count={62} />
-      <PostInteraction navigation={navigation} icon_name="share" count={250} />
-      <PostInteraction navigation={navigation} icon_name="comment" count={18} />
+      <PostHeader user={post.user} />
+      <PostImage post={post} />
+      <PostFooter navigation={navigation} post={post} />
+    </View>
+  );
+};
+
+const PostHeader = ({ user }) => {
+  return (
+    <View style={styles.headerContainer}>
+      <Image
+        source={{
+          uri: user.profile,
+        }}
+        style={styles.profileImage}
+      />
+      <View style={styles.innerHeaderContainer}>
+        <Text style={{ fontWeight: 600 }}>{user.name}</Text>
+        <MaterialCommunityIcons
+          name="dots-horizontal"
+          size={24}
+          color="black"
+        />
+      </View>
+    </View>
+  );
+};
+
+const PostImage = ({ post }) => {
+  return (
+    <View>
+      <Image source={{ uri: post.image }} style={styles.image} />
+      <Text style={{ ...styles.primaryText, marginTop: -100 }}>
+        {post.title}
+      </Text>
+      <Text style={styles.secondaryText}>
+        <FontAwesome name="map-marker" size={12} /> {post.location}
+      </Text>
+      <Text style={styles.secondaryText}>
+        <FontAwesome name="calendar-o" size={12} /> {post.date}
+      </Text>
+    </View>
+  );
+};
+
+const PostFooter = ({ navigation, post }) => {
+  return (
+    <View style={styles.footerContainer}>
+      <PostInteraction
+        navigation={navigation}
+        icon_name="hop"
+        count={post.likes}
+      />
+      <PostInteraction
+        navigation={navigation}
+        icon_name="share"
+        count={post.shares}
+      />
+      <PostInteraction
+        navigation={navigation}
+        icon_name="comment"
+        count={post.comments}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginBottom: 20,
+  },
   imageContainer: {},
   title: {},
+  headerContainer: {
+    flexDirection: "row",
+    height: 50,
+    alignItems: "center",
+  },
+  profileImage: {
+    height: 40,
+    width: 40,
+    borderRadius: 100,
+    marginLeft: 10,
+  },
+  innerHeaderContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  primaryText: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: 600,
+    marginLeft: 20,
+  },
+  secondaryText: {
+    color: "white",
+    marginLeft: 20,
+    fontWeight: 500,
+  },
+  footerContainer: {
+    flexDirection: "row",
+    marginTop: 40,
+    width: "100%",
+    justifyContent: "space-around",
+  },
 });
 
 export default FeedPost;
--- mobile/src/components/PostInteraction.js
+++ mobile/src/components/PostInteraction.js
@@ -11,10 +11,12 @@ const PostInteraction = ({
   count,
 }) => {
   const [flyState, setFlyState] = useState(active);
+  const [increment, setIncrement] = useState(0);
 
   const onClick = () => {
     if (icon_name.includes("hop")) {
       setFlyState(!flyState);
+      setIncrement(!Boolean(increment) ? 1 : 0);
     } else if (icon_name == "share") {
       navigation.navigate("Itinerary");
     }
@@ -22,17 +24,17 @@ const PostInteraction = ({
 
   const icons = {
     hop: flyState ? (
-      <Ionicons name="md-airplane" size={26} color="black" />
+      <Ionicons name="md-airplane" size={26} color={colors.primary} />
     ) : (
       <Ionicons name="md-airplane-outline" size={26} color="black" />
     ),
     share: <Ionicons name="share-social-outline" size={26} color="black" />,
-    comment: <EvilIcons name="comment" size={26} color="black" />,
+    comment: <EvilIcons name="comment" size={35} color="black" />,
   };
   return (
     <TouchableOpacity style={styles.container} onPress={onClick}>
       {icons[icon_name]}
-      <Text style={styles.number}>{count}</Text>
+      <Text style={styles.number}>{count + increment}</Text>
     </TouchableOpacity>
   );
 };
